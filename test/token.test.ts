ceb4a7faa21741fa7448a9578fc72ebba7027247
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2022-07-22 09:03:46 +0300

    Fixed test

--- test/token.test.ts
+++ test/token.test.ts
@@ -895,9 +895,7 @@ describe("Token", () => {
     it("sending Eth to the contract fails", async () => {
       await expect(
         user1.sendTransaction({ to: erc20.address, value: 5 })
-      ).to.revertedWith(
-        "Transaction reverted: function selector was not recognized and there's no fallback nor receive function"
-      );
+      ).to.revertedWith("Transaction reverted without a reason string");
     });
 
     it("sending contract's own tokens to the contract fails", async () => {
