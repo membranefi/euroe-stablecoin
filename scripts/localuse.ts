dff57b7c7b3bad509b75c2e9adc8d0bb298adfa0
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2022-11-07 08:34:28 +0200

    document script

--- scripts/localuse.ts
+++ scripts/localuse.ts
@@ -12,8 +12,9 @@ import { splitSignatureToRSV } from "./eth-permit/rpc";
 
 Env.config({ path: "./.secrets.env" });
 
-// Used for deploying directly to Goerli
 async function main() {
+  // SET PARAMETERS
+
   const accounts = await ethers.getSigners();
   const proxyOwner = accounts[0],
     admin = proxyOwner,
@@ -24,6 +25,16 @@ async function main() {
     rescuer = proxyOwner,
     burner = proxyOwner;
 
+  const deadline = 1967397081;
+  const jsonProvider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:8545"
+  );
+  const nonce = 0;
+  const amount = 2;
+  const spenderAddress = accounts[1].address;
+  const walletAddress = proxyOwner.address;
+
+  // DEPLOY
   const Token = await ethers.getContractFactory("EUROe");
   const deployment = await upgrades.deployProxy(
     Token,
@@ -47,16 +58,7 @@ async function main() {
   console.log("Token address:", implementation);
   console.log("Proxy address:", proxy.address);
 
-  const deadline = 1967397081;
-  const jsonProvider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:8545"
-  );
-  const nonce = 0;
-  const amount = 2;
-  const spenderAddress = accounts[1].address;
-
-  const jsonSigner = await jsonProvider.getSigner(0);
-  const walletAddress = proxyOwner.address; //await jsonSigner.getAddress();
+  // FORM DATA TO SIGN
 
   const domain: Domain = {
     name: "EURO Stablecoin",
@@ -69,19 +71,23 @@ async function main() {
     owner: walletAddress,
     spender: spenderAddress,
     value: amount,
-    //nonce: nonce === undefined ? await call(provider, tokenAddress, `${NONCES_FN}${zeros(24)}${vaultAddress.substr(2)}`) : nonce,
     nonce: nonce,
     deadline: deadline,
   };
 
   const typedData = createTypedERC2612Data(message, domain);
-  //console.log("Typed", typedData);
+
   const json = JSON.stringify(typedData);
+
+  // SIGN DATA
+
   const rpcResult = await jsonProvider.send("eth_signTypedData_v4", [
     walletAddress,
     json,
   ]);
   const rpxSig = splitSignatureToRSV(rpcResult);
+
+  // USE SIGNATURE TO CREATE ALLOWANCE AND CHECK THAT IT WORKS
 
   const allBefore = await proxy.allowance(walletAddress, spenderAddress);
   await proxy.permit(
@@ -94,31 +100,8 @@ async function main() {
     rpxSig.s
   );
   const allAfter = await proxy.allowance(walletAddress, spenderAddress);
+  // Should output '0 2'
   console.log("allowances", allBefore.toString(), allAfter.toString());
-
-  if (network.name !== "hardhat" && network.name !== "localhost") {
-    console.log("Deployments done, waiting for etherscan verifications");
-    // Wait for the contracts to be propagated inside Etherscan
-    await new Promise((f) => setTimeout(f, 60000));
-
-    // Verify in scanner
-    try {
-      await hre.run("verify:verify", {
-        address: implementation,
-      });
-    } catch (ex: unknown) {
-      if (String(ex).indexOf("already verified") == -1) {
-        // verified probably because it has the same bytecode as some other contract
-        throw ex;
-      }
-    }
-
-    console.log(
-      "Contracts verified. You can now go to contract at " +
-        proxy.address +
-        " and mark it as proxy"
-    );
-  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
