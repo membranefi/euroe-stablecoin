7fb60d1c3de8291d1fe45b4b4d1d166e09e6a486
Juuso Roinevirta <jroinevirta@gmail.com>
2023-01-04 11:54:03 +0200

    Fix copyright year

--- contracts/EUROe.sol
+++ contracts/EUROe.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 /*
-Copyright (c) 2022 Membrane Finance Oy
+Copyright (c) 2023 Membrane Finance Oy
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
